9e0a2501aadfbaac47f862edb9ae625e64098bb6
Luong Trung <trunghlbuh@gmail.com>
2021-12-03 17:00:00 +0700

    [FE] settings display home screen

--- learn-rxjs/src/app/rxjs-home/rxjs-home.component.ts
+++ learn-rxjs/src/app/rxjs-home/rxjs-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-rxjs-home',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rxjs-home.component.scss']
 })
 export class RxjsHomeComponent implements OnInit {
-
-  constructor() { }
+  listItems: any;
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
+    this.listItems = [
+      {
+        name: 'Subjects',
+        link: '/rxjs-subject'
+      },
+      {
+        name: 'Mergin Observables',
+        link: '#'
+      },
+      {
+        name: 'Memory Leaks',
+        link: '#'
+      },
+      {
+        name: 'Mergin Observables',
+        link: '#'
+      },
+      {
+        name: 'RxJS features',
+        link: '#'
+      },
+      {
+        name: 'RxJS uses Pattern',
+        link: '#'
+      },
+      {
+        name: 'Observables into Array',
+        link: '#'
+      }
+    ]
   }
-
+  
+  onNavigate(url: string): void {
+    this.router.navigateByUrl(url);
+  }
 }
